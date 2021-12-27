2fac64ef47f959b4797eecfb2bc2ac9bd716b014
ixmorrow <ivanmorrow14@gmail.com>
2021-12-27 12:05:19 -0600

    added some security checks on updating riddles and some more tests

--- programs/riddle-app/src/lib.rs
+++ programs/riddle-app/src/lib.rs
@@ -4,32 +4,39 @@ use anchor_lang::prelude::*;
 declare_id!("BRgx1ATbc2rP9ge3k2RyCi3RWxNzbaqaveeSCeX8ZzhT");
 
 #[program]
-pub mod riddle_app {
+pub mod riddleapp {
+    
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, riddle: String, answer: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         base_account.riddle = riddle;
         base_account.answer = answer;
+        base_account.is_correct = false;
         Ok(())
     }
 
     pub fn update(ctx: Context<Update>, riddle: String, answer: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.riddle = riddle;
-        base_account.answer = answer;
+
+        if base_account.is_correct{
+            base_account.riddle = riddle;
+            base_account.answer = answer;
+            base_account.is_correct = false;
+        }
+
         Ok(())
     }
 
-    // pub fn check(ctx: Context<Check>, answer: String) -> bool {
-    //     let base_account = &ctx.accounts.base_account;
+    pub fn check(ctx: Context<Check>, answer: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
 
-    //     match answer {
-    //         base_account.answer => true,
-    //         _ => false
-    //     }
+        if base_account.answer == answer{
+            base_account.is_correct = true;
+        }
 
-    // }
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -48,15 +55,17 @@ pub struct Update<'info> {
 }
 
 //account to check user answer, wanted to seperate this from client side so it doesn't directly access the correct answer
-// #[derive(Accounts)]
-// pub struct Check<'info> {
-//     #[account]
-//     pub base_account: Account<'info, BaseAccount>,
-// }
+#[derive(Accounts)]
+pub struct Check<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
 
 #[account]
 pub struct BaseAccount {
     //pub data: String,
     pub riddle: String,
-    pub answer: String,
+    //change this to private once I'm done testing so that no one can access it and cheat
+    answer: String,
+    is_correct: bool,
 }
--- tests/riddle-app.ts
+++ tests/riddle-app.ts
@@ -25,10 +25,43 @@ describe('riddleapp', () => {
     //console.log('Data: ', account.riddle);
     assert.ok(account.riddle === "What gets wet while drying?");
     assert.ok(account.answer === "A towel");
-
+    assert.ok(account.isCorrect === false);
   });
 
-  it("Updating initialized riddle account", async function() {
+  it("Checks incorrect answer", async function() {
+    await program.rpc.check("a fish", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.isCorrect === false);
+  })
+
+  it("Tries to update whithout correct answer", async function() {
+    await program.rpc.update("Until I am measured, I am not known. Yet how you miss me, When I have flown.", "Time", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.riddle === "What gets wet while drying?");
+  })
+
+  it("Gives correct answer to riddle", async function() {
+    await program.rpc.check("A towel", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    })
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.isCorrect === true);
+  })
+
+  it("Updating riddle account after correctly answering initial riddle", async function() {
     await program.rpc.update("Until I am measured, I am not known. Yet how you miss me, When I have flown.", "Time", {
       accounts: {
         baseAccount: baseAccount.publicKey,
@@ -37,7 +70,19 @@ describe('riddleapp', () => {
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
     assert.ok(account.riddle === "Until I am measured, I am not known. Yet how you miss me, When I have flown.");
-    assert.ok(account.answer === "Time");
+    //assert.ok(account.answer === "Time");
+    assert.ok(account.isCorrect === false);
+  })
+
+  it("Check incorrect riddle after updating new riddle", async function() {
+    await program.rpc.check("a fish", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    })
+  
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.isCorrect === false);
   })
 
 })
